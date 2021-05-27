90bb20cbdd6f47761fb78d6c584fb817aee209e3
Alex Good <alex@memoryandthought.me>
2021-05-27 15:49:12 +0100

    Point at patched libgit
    Also fix a number of clippy lints which were activated by the change
    in the rust-toolchain
    
    Signed-off-by: Alex Good <alex@memoryandthought.me>

--- proxy/api/src/process.rs
+++ proxy/api/src/process.rs
@@ -46,7 +46,7 @@ struct Rigging {
     /// The context provided to the API
     ctx: context::Context,
     /// The [`Peer`] to run
-    peer: Option<Peer<radicle_daemon::config::StreamDiscovery>>,
+    peer: Option<Peer<signer::BoxedSigner, radicle_daemon::config::StreamDiscovery>>,
     /// Channel to receive updates to the seed nodes from the API
     seeds_sender: Option<watch::Sender<Vec<seed::Seed>>>,
 }
